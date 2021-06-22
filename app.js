911f97e65341116dac3c2333a186aa44688fc4de
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-21 20:52:49 -0500

    adding dependencies for pasing and authentication

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ const express = require('express');
 const app = express();
 const Handlebars = require('handlebars');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+// const checkAuth = require('./middleware/checkAuth');
+
+// environment variables
 require('dotenv').config();
+
+// body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }), methodOverride('_method'));
 
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 
 // middleware
 var exphbs = require('express-handlebars');
+// middleware for parsing cookies
+app.use(cookieParser());
+
+// user authentication
+// app.use(checkAuth); 
 
 // static files
 app.use(express.static('public'));
@@ -17,6 +31,7 @@ app.engine('handlebars', exphbs({defaultLayout: 'main',
 handlebars: allowInsecurePrototypeAccess(Handlebars)}));
 app.set('view engine', 'handlebars');
 
+// controllers
 require('./controllers/books.js')(app);
 
 // define app route
