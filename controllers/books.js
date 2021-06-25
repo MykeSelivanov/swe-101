3965c438ee401a8320ebd787df43b4a8c5b48577
MykeSelivanov <michael777selivanov@gmail.com>
2021-06-25 00:09:51 -0500

    adding route for displaying single book

--- controllers/books.js
+++ controllers/books.js
@@ -12,8 +12,8 @@ module.exports = function(app) {
     app.get('/books', (req, res) => {
         // query parameter vs params
         const param = req.query.category;
-        console.log(param);
-        if(param  === undefined) {
+    
+        if(param  === 'all' || param === undefined) {
             Book.find()
             .then(books => {
                 res.render('books-index', { books });
@@ -22,9 +22,8 @@ module.exports = function(app) {
                 console.log(err);
             });
         } else {
-            Book.find({ category: param})
+            Book.find({ category: param })
             .then(books => {
-                console.log(books)
                 res.render('books-index', { books });
             })
             .catch(err => {
@@ -33,8 +32,20 @@ module.exports = function(app) {
         }
     });
 
+    // Form for addin a new book
     app.get('/books/new', (req, res) => {
         res.render('books-new');
+    });
+
+    // Getting single book
+    app.get('/books/:id', (req, res) => {
+        Book.findById(req.params.id).lean()
+        .then(book => {
+            res.render('books-show', { book });
+        })
+        .catch(err => {
+            console.log(err);
+        });
     });
 
     // Create new book
